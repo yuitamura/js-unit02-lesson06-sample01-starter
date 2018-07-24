5feb85009ec8fb23490c4fb7174a40694f23bcce
Atsuhiro Teshima <hiro@lingualbox.com>
2018-07-24 13:26:35 +0800

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'webpack-hot-middleware/client',
+        './src/index.js'
+    ],
+    output: {
+        filename: './bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        hot: true,
+        hotOnly: true,
+        watchContentBase: true,
+        watchOptions: {
+            ignored: /node_modules/
+        }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+};
