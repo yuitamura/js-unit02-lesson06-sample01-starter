1dd9c9dc5d2ef40ad83d98451cfedcab64cbe5f2
Atsuhiro Teshima <hiro@lingualbox.com>
2018-10-23 20:18:33 +0800

    Modify indent size and upgrade bootstrap

--- server.js
+++ server.js
@@ -7,47 +7,47 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.use(require("webpack-dev-middleware")(compiler, {
-    noInfo: true, publicPath: webpackConfig.output.publicPath
+  noInfo: true, publicPath: webpackConfig.output.publicPath
 }));
 
 app.use(require("webpack-hot-middleware")(compiler));
 
 app.post('/login', (req, res) => {
-    const { email, password } = req.body
-    if (email === 'testuser@codegrit.jp' && password === 'password') {
-        res.status(200).send(JSON.stringify({
-            "id": 1,
-            "name": "Code Grit",
-            "email": "testuser@codegrit.jp",
-            "message": "ログイン成功！"
-        }));
-    } else {
-        res.status(401).send(JSON.stringify({
-            message: "ログイン失敗"
-        }))
-    }
+  const { email, password } = req.body
+  if (email === 'testuser@codegrit.jp' && password === 'password') {
+    res.status(200).send(JSON.stringify({
+      "id": 1,
+      "name": "Code Grit",
+      "email": "testuser@codegrit.jp",
+      "message": "ログイン成功！"
+    }));
+  } else {
+    res.status(401).send(JSON.stringify({
+      message: "ログイン失敗"
+    }))
+  }
 });
 
 app.get('/users/:userId', (req, res) => {
-    const userId = parseInt(req.params.userId);
-    if (userId === 1) {
-        res.send({
-            "id": 1,
-            "name": "Code Grit",
-            "email": "testuser@codegrit.jp"
-        });
-    } else {
-        console.log(userId);
-        res.status(403).send({
-            error: "No resource found."
-        });
-    }
+  const userId = parseInt(req.params.userId);
+  if (userId === 1) {
+    res.send({
+      "id": 1,
+      "name": "Code Grit",
+      "email": "testuser@codegrit.jp"
+    });
+  } else {
+    console.log(userId);
+    res.status(403).send({
+      error: "No resource found."
+    });
+  }
 });
 
 app.listen(3000, () => console.log('listening on port 3000!'));
